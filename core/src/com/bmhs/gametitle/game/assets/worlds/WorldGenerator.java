3a4f41cc4278bdacc1529dfc59581927f9d76847
BEEEG-YOSHI <lukethomasprice@me.com>
2024-01-25 13:15:24 -0800

    Commit #3

--- core/src/com/bmhs/gametitle/game/assets/worlds/WorldGenerator.java
+++ core/src/com/bmhs/gametitle/game/assets/worlds/WorldGenerator.java
@@ -2,7 +2,10 @@ package com.bmhs.gametitle.game.assets.worlds;
 
 import com.badlogic.gdx.Gdx;
 
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
 import com.bmhs.gametitle.gfx.assets.tiles.statictiles.WorldTile;
 import com.bmhs.gametitle.gfx.utils.TileHandler;
 
@@ -10,7 +13,6 @@ import com.bmhs.gametitle.gfx.utils.TileHandler;
 public class WorldGenerator {
 
     private int worldMapRows, worldMapColumns;
-    private int temp;
     private int[][] worldIntMap;
     private int waterColor;
     private  int strength;
@@ -22,17 +24,31 @@ public class WorldGenerator {
 
         worldIntMap = new int[worldMapRows][worldMapColumns];
 
+        /*Vector2 mapSeed = new Vector2(MathUtils.random(worldIntMap[0].length), MathUtils.random(worldIntMap.length));
+        System.out.println(mapSeed.y + " "+ mapSeed.x);
+
+        worldIntMap[(int)mapSeed.x][(int)mapSeed.y] = 4;
+
+        for(int r = 0;r < worldIntMap.length; r++){
+            for(int c = 0; c < worldIntMap[r].length; c++){
+                Vector2 tempVector = new Vector2(r,c);
+                if(tempVector.dst(mapSeed) < 10){
+                    worldIntMap[r][c] = 2;
+                }
+            }
+        }*/
+
         //call methods to build 2D array
         //randomize();
         iniWater();
-        border();
         generateIslands();
         Gdx.app.error("WorldGenerator", "WorldGenerator(WorldTile[][][])");
+        generateWorldTextFile();
     }
 
     public void generateIslands(){
-        int iR = MathUtils.random(3,96);
-        int iC = MathUtils.random(3,196);
+        int iR = MathUtils.random(3,worldIntMap.length - 4);
+        int iC = MathUtils.random(3,worldIntMap[0].length - 4);
         strength = 9;
         //int strength = MathUtils.random();
         worldIntMap[iR][iC] = strength;
@@ -47,21 +63,23 @@ public class WorldGenerator {
     }
 
     public void recolor(int iR, int iC){
-        if(worldIntMap[iR+1][iC] > worldIntMap[iR][iC] || worldIntMap[iR+1][iC] == waterColor){
-            worldIntMap[iR+1][iC] = worldIntMap[iR][iC]-1;
-            island(iR+1,iC,strength-1);
-        }
-        if(worldIntMap[iR-1][iC] > worldIntMap[iR][iC] || worldIntMap[iR-1][iC] == waterColor){
-            worldIntMap[iR-1][iC] = worldIntMap[iR][iC]-1;
-            island(iR-1,iC,strength-1);
-        }
-        if(worldIntMap[iR][iC+1] > worldIntMap[iR][iC] || worldIntMap[iR][iC+1] == waterColor){
-            worldIntMap[iR][iC+1] = worldIntMap[iR][iC]-1;
-            island(iR,iC+1,strength-1);
-        }
-        if(worldIntMap[iR][iC-1] > worldIntMap[iR][iC] || worldIntMap[iR][iC-1] == waterColor){
-            worldIntMap[iR][iC-1] = worldIntMap[iR][iC]-1;
-            island(iR,iC-1,strength-1);
+        if(iR > 1 && iR < 99 && iC > 1 && iC < 199) {
+            if (/*worldIntMap[iR + 1][iC] < worldIntMap[iR][iC] &&*/ worldIntMap[iR + 1][iC] == waterColor) {
+                worldIntMap[iR + 1][iC] = worldIntMap[iR][iC] - 1;
+                island(iR + 1, iC, strength - 1);
+            }/*
+            if (worldIntMap[iR - 1][iC] < worldIntMap[iR][iC] && worldIntMap[iR - 1][iC] == waterColor) {
+                worldIntMap[iR - 1][iC] = worldIntMap[iR][iC] - 1;
+                island(iR - 1, iC, strength - 1);
+            }
+            if (worldIntMap[iR][iC + 1] < worldIntMap[iR][iC] && worldIntMap[iR][iC + 1] == waterColor) {
+                worldIntMap[iR][iC + 1] = worldIntMap[iR][iC] - 1;
+                island(iR, iC + 1, strength - 1);
+            }
+            if (worldIntMap[iR][iC - 1] < worldIntMap[iR][iC] && worldIntMap[iR][iC - 1] == waterColor) {
+                worldIntMap[iR][iC - 1] = worldIntMap[iR][iC] - 1;
+                island(iR, iC - 1, strength - 1);
+            }*/
         }
     }
 
@@ -89,16 +107,11 @@ public class WorldGenerator {
         return returnString;
     }
 
-    public void border() {
-        for(int r = 0; r < worldIntMap.length; r++) {
-            for(int c = 0; c < worldIntMap[r].length; c++){
-                if(c < 2 || c > worldIntMap[r].length - 3 || r < 2 || r > worldIntMap.length - 3) {
-                    worldIntMap[r][c] = 14;
-                }
-            }
-
-        }
+    private void generateWorldTextFile() {
+        FileHandle file = Gdx.files.local("assets/worlds/world.text");
+        file.writeString(getWorld3DArrayToString(), false);
     }
+
 
     public void randomize() {
         for(int r = 0; r < worldIntMap.length; r++) {
